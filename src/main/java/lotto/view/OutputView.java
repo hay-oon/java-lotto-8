c3bd94c8131a7d11a8f05dde2182a29d8314a783
hay-oon <hayooncode@gmail.com>
2025-11-02 01:12:10 +0900

    feat: OutputView 클래스 생성 및 기본 메서드 시그니처 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,39 @@
 package lotto.view;
 
+import lotto.model.LottoTicket;
+import lotto.model.WinningNumber;
+import lotto.model.LottoResult;
+import lotto.model.EarningRate;
+
 public class OutputView {
+    private static final String PURCHASE_NUMBER_OUTPUT_MESSAGE = "%d개를 구매했습니다.";
+    private static final String WINNING_RESULT_OUTPUT_MESSAGE = "당첨 통계";
+    private static final String WINNING_RESULT_SEPARATOR = "---";
+    private static final String WINNING_RESULT_3_MATCH_MESSAGE = "3개 일치 (5,000원) - %d개";
+    private static final String WINNING_RESULT_4_MATCH_MESSAGE = "4개 일치 (50,000원) - %d개";
+    private static final String WINNING_RESULT_5_MATCH_MESSAGE = "5개 일치 (1,500,000원) - %d개";
+    private static final String WINNING_RESULT_5_MATCH_BONUS_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    private static final String WINNING_RESULT_6_MATCH_MESSAGE = "6개 일치 (2,000,000,000원) - %d개";
+    private static final String EARNING_RATE_OUTPUT_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
+
+    public static void printPurchaseNumber(Integer purchaseNumber) {
+        System.out.println(String.format(PURCHASE_NUMBER_OUTPUT_MESSAGE, purchaseNumber));
+    }
+
+    public static void printLottoTicket(LottoTicket lottoTicket) {
+        System.out.println(lottoTicket.getLottoNumbers());
+    }
+
+    public static void printResult(LottoResult lottoResult, Double earningRate) {
+        System.out.println(WINNING_RESULT_OUTPUT_MESSAGE);
+        System.out.println(WINNING_RESULT_SEPARATOR);
+        //TODO: 당첨 결과 출력 수정
+        System.out.println(String.format(WINNING_RESULT_3_MATCH_MESSAGE, lottoResult.get()));
+        System.out.println(String.format(WINNING_RESULT_4_MATCH_MESSAGE, lottoResult.get()));
+        System.out.println(String.format(WINNING_RESULT_5_MATCH_MESSAGE, lottoResult.get()));
+        System.out.println(String.format(WINNING_RESULT_5_MATCH_BONUS_MESSAGE, lottoResult.get()));
+        System.out.println(String.format(WINNING_RESULT_6_MATCH_MESSAGE, lottoResult.get()));
+        System.out.println(String.format(EARNING_RATE_OUTPUT_MESSAGE, earningRate));
+    }
 }
