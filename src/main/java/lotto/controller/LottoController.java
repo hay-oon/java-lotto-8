9d392bd42571da760cabfb17a65fc6115818c070
hay-oon <hayooncode@gmail.com>
2025-11-01 23:48:12 +0900

    feat: 사용자 흐름에 따라 컨트롤러 작성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,29 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import lotto.model.LottoTicket;
+import lotto.model.WinningNumber;
+import lotto.model.LottoResult;
+import lotto.model.EarningRate;
+
+public class LottoController {
+    public void run() {
+        Integer purchaseNumber = InputView.readPurchaseAmount();
+        LottoTicket lottoTicket = LottoTicket.generateTicket(purchaseNumber);
+
+        OutputView.printPurchaseNumber(purchaseNumber);
+        OutputView.printLottoTicket(lottoTicket);
+
+        List<Integer> winningNumbers = InputView.readWinningNumber();
+        Integer bonusNumber = InputView.readBonusNumber();
+
+        WinningNumber winningNumberObj = new WinningNumber(winningNumbers, bonusNumber);
+
+        LottoResult lottoResult = LottoResult.matchNumber(lottoTicket, winningNumberObj);
+
+        EarningRate earningRate = EarningRate.calculateEarningRate(lottoResult, purchaseNumber);
+        OutputView.printResult(lottoResult, earningRate.getEarningRate());
+    }
+}
