cc7c77b889e278a774dff74af09a1fdbaa40bd39
hay-oon <hayooncode@gmail.com>
2025-11-03 01:17:37 +0900

    refactor: 에러 메시지 출력 형식 요구사항에 맞게 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -7,9 +7,9 @@ public class Lotto {
     private static final int LOTTO_NUMBER_SIZE = 6;
     private static final int LOTTO_NUMBER_MIN = 1;
     private static final int LOTTO_NUMBER_MAX = 45;
-    private static final String NUMBER_SIZE_ERROR_MESSAGE = "로또 번호는 " + LOTTO_NUMBER_SIZE + "개여야 합니다.";
-    private static final String NUMBER_RANGE_ERROR_MESSAGE = "로또 번호는 " + LOTTO_NUMBER_MIN + "~" + LOTTO_NUMBER_MAX + " 사이의 숫자여야 합니다.";
-    private static final String NUMBER_DUPLICATE_ERROR_MESSAGE = "로또 번호는 " + LOTTO_NUMBER_SIZE + "개의 중복되지 않는 숫자여야 합니다.";
+    private static final String NUMBER_SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호는 " + LOTTO_NUMBER_SIZE + "개여야 합니다.";
+    private static final String NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 " + LOTTO_NUMBER_MIN + "~" + LOTTO_NUMBER_MAX + " 사이의 숫자여야 합니다.";
+    private static final String NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 " + LOTTO_NUMBER_SIZE + "개의 중복되지 않는 숫자여야 합니다.";
 
     private final List<Integer> numbers;
 
@@ -42,7 +42,7 @@ public class Lotto {
     private static List<Integer> sortNumbersAscending(List<Integer> numbers) {
         return numbers.stream().sorted().toList();
     }
-    
+
     @Override
     public String toString() {
         return numbers.toString();
--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -6,14 +6,14 @@ public class WinningNumber {
     private static final int WINNING_NUMBERS_SIZE = 6;
     private static final int WINNING_NUMBER_MIN = 1;
     private static final int WINNING_NUMBER_MAX = 45;
-    private static final String WINNING_NUMBERS_SIZE_ERROR_MESSAGE = "당첨 번호는 " + WINNING_NUMBERS_SIZE + "개여야 합니다.";
+    private static final String WINNING_NUMBERS_SIZE_ERROR_MESSAGE = "[ERROR] 당첨 번호는 " + WINNING_NUMBERS_SIZE + "개여야 합니다.";
     private static final String WINNING_NUMBER_RANGE_ERROR_MESSAGE =
-            "당첨 번호는 " + WINNING_NUMBER_MIN + "~" + WINNING_NUMBER_MAX + " 사이의 숫자여야 합니다.";
+            "[ERROR] 당첨 번호는 " + WINNING_NUMBER_MIN + "~" + WINNING_NUMBER_MAX + " 사이의 숫자여야 합니다.";
     private static final String WINNING_NUMBERS_DUPLICATE_ERROR_MESSAGE =
-            "당첨 번호는 " + WINNING_NUMBERS_SIZE + "개의 중복되지 않는 숫자여야 합니다.";
+            "[ERROR] 당첨 번호는 " + WINNING_NUMBERS_SIZE + "개의 중복되지 않는 숫자여야 합니다.";
     private static final String BONUS_NUMBER_RANGE_ERROR_MESSAGE =
-            "보너스 번호는 " + WINNING_NUMBER_MIN + "~" + WINNING_NUMBER_MAX + " 사이의 숫자여야 합니다.";
-    private static final String DUPLICATE_NUMBER_ERROR_MESSAGE = "보너스 번호는 당첨번호와 중복될 수 없습니다.";
+            "[ERROR] 보너스 번호는 " + WINNING_NUMBER_MIN + "~" + WINNING_NUMBER_MAX + " 사이의 숫자여야 합니다.";
+    private static final String DUPLICATE_NUMBER_ERROR_MESSAGE = "[ERROR] 보너스 번호는 당첨번호와 중복될 수 없습니다.";
 
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
