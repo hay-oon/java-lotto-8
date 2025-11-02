a1996c3ccbe156103c1c5d73d904bf309e9413ec
hay-oon <hayooncode@gmail.com>
2025-11-02 16:48:24 +0900

    feat: LottoTicket 클래스 추가 및 구매 개수에 따른 로또 티켓 생성 기능 구현

---
+++ src/main/java/lotto/model/LottoTicket.java
@@ -0,0 +1,22 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class LottoTicket {
+    private final List<Lotto> lottoList;
+    private final Integer purchaseNumber;
+
+    public LottoTicket(List<Lotto> lottoList, Integer purchaseNumber) {
+        this.lottoList = lottoList;
+        this.purchaseNumber = purchaseNumber;
+    }
+
+    public static LottoTicket fromPurchaseNumber(Integer purchaseNumber) {
+        List<Lotto> lottoList = new ArrayList<Lotto>();
+        for (int i = 0; i < purchaseNumber; i++) {
+            lottoList.add(Lotto.fromRandomNumbers());
+        }
+        return new LottoTicket(lottoList, purchaseNumber);
+    }
+}
