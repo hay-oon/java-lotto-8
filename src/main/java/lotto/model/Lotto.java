355ec9ea8be78cb8c652d4807810006703df17db
hay-oon <hayooncode@gmail.com>
2025-11-02 16:01:03 +0900

    feat: Lotto 클래스 내 로또 번호 생성 메서드 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
-package lotto;
+package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +17,15 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public static Lotto generateLotto() {
+        return new Lotto(generateRandomNumbers());
+    }
+
+    private static List<Integer> generateRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
 }
