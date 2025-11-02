141b5907c9dc80c4c6b6f535012d397f6e6a77f8
hay-oon <hayooncode@gmail.com>
2025-11-02 17:52:01 +0900

    feat: 구매 금액 유효성 검사 메시지 개선 및 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,7 +8,8 @@ public class InputView {
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
-    private static final String PURCHASE_AMOUNT_ERROR_MESSAGE = "[ERROR] 구입 금액은 0 이하일 수 없습니다.";
+    private static final String PURCHASE_AMOUNT_NOT_ZERO_ERROR_MESSAGE = "[ERROR] 구입 금액은 0 이하일 수 없습니다.";
+    private static final String PURCHASE_AMOUNT_NOT_1000_UNIT_ERROR_MESSAGE = "[ERROR] 구입 금액은 1000원 단위여야 합니다.";
 
     public static Integer readPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
@@ -37,7 +38,10 @@ public class InputView {
 
     private static void validatePurchaseAmount(Integer purchaseAmount) {
         if (purchaseAmount <= 0) {
-            throw new IllegalArgumentException(PURCHASE_AMOUNT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_NOT_ZERO_ERROR_MESSAGE);
+        }
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_NOT_1000_UNIT_ERROR_MESSAGE);
         }
     }
 }
