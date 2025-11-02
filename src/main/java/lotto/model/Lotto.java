7b3be38a95ab9d36ae55ae46b492f41d559905d9
hay-oon <hayooncode@gmail.com>
2025-11-02 16:47:07 +0900

    refactor: Lotto 클래스의 유효성 검사 로직 개선 및 상수화

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -4,6 +4,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+    private static final String NUMBER_SIZE_ERROR_MESSAGE = "로또 번호는 " + LOTTO_NUMBER_SIZE + "개여야 합니다.";
+    private static final String NUMBER_RANGE_ERROR_MESSAGE = "로또 번호는 " + LOTTO_NUMBER_MIN + "~" + LOTTO_NUMBER_MAX + " 사이의 숫자여야 합니다.";
+    private static final String NUMBER_DUPLICATE_ERROR_MESSAGE = "로또 번호는 " + LOTTO_NUMBER_SIZE + "개의 중복되지 않는 숫자여야 합니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,8 +19,14 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException(NUMBER_SIZE_ERROR_MESSAGE);
+        }
+        if (numbers.stream().anyMatch(number -> number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX)) {
+            throw new IllegalArgumentException(NUMBER_RANGE_ERROR_MESSAGE);
+        }
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_SIZE) {
+                throw new IllegalArgumentException(NUMBER_DUPLICATE_ERROR_MESSAGE);
         }
     }
 
