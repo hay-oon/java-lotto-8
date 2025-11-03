3965fc9d637d450a4b567f1e27e1d55e5a3f8e99
hay-oon <hayooncode@gmail.com>
2025-11-03 14:48:58 +0900

    test: 로또 번호 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,5 +23,22 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("로또 번호가 1~45 범위를 벗어나면 예외가 발생한다")
+    void 로또_번호가_범위를_벗어나면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+        
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호가 오름차순으로 정렬된다")
+    void 로또_번호가_오름차순으로_정렬된다() {
+        Lotto lotto = new Lotto(List.of(6, 3, 1, 5, 2, 4));
+        List<Integer> numbers = lotto.getNumbers();
+        
+        assertThat(numbers).containsExactly(1, 2, 3, 4, 5, 6);
+    }
 }
